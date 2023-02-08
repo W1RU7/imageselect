e500f115376fdd7082b104f158fe2b930141dac9
W1RU7 <100665897+W1RU7@users.noreply.github.com>
2023-02-08 15:21:59 +0200

    Add files via upload

---
+++ Contact/js/main.js
@@ -0,0 +1,25 @@
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "231px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
